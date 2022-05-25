7689b5235cebbcc2529ec8309bfaca4e4e29e59d
gideonopa <gideonoparinde@gmail.com>
2022-05-25 07:31:55 +0000

    Printf

--- _printf.c
+++ _printf.c
@@ -1,49 +1,50 @@
 #include "main.h"
 
 /**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
+
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+int (*pfunc)(va_list, flags_t *);
+const char *p;
+va_list arguments;
+flags_t flags = {0, 0, 0};
 
-	va_start(ap, format);
+register int counter = 0;
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
+va_start(arguments, format);
+if (!format || (format[0] == '%' && !format[1]))
+	return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+	return (-1);
+for (p = format; *p; p++)
+{
+	if (*p == '%')
 	{
-		init_params(&params, ap);
-		if (*p != '%')
-		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
+	p++;
+	if (*p == '%')
+	{
+		counter += _putchar('%');
+		continue;
 	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+	while (get_flag(*p, &flags))
+		p++;
+	pfunc = get_print(*p);
+	counter += (pfunc)
+			? pfunc(arguments, &flags)
+			: _printf("%%%c", *p);
+	}
+	else
+	counter += _putchar(*p);
+}
+
+_putchar(-1);
+va_end(arguments);
+return (counter);
 }
